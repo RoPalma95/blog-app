1740f2a2d12a0803eb879c0c8fb996f6558f1123
RoPalma95 <r.palma995@gmail.com>
2021-03-10 15:19:31 -0600

    Finished tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_183553) do
+ActiveRecord::Schema.define(version: 2021_03_10_204235) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -23,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_183553) do
     t.string "commenter"
     t.text "body"
     t.integer "article_id", null: false
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
